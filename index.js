05d126372a334a4b9663345fa091c3c8e92bbc5e
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-04-02 20:26:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+let cards= ( "Lisa", "Kaitlin", "Jan);
+
+function writeCards(cards) {
+  for(let i=0; i<cards.ligeth; i++){
+    console.log(`Thank you ${cards[i]} for the wonderful birthday gift!`)
+  }
+  return cards;
+}
+writeCards(cards);
+
