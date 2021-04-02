e7b2754b3dce86f1d0aa4b84466f8fe68ecae05f
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-04-02 21:37:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function writeCards(cards, event) {
   let eventName;
   let finalEvent = [];
   for(let i=0; i<cards.length; i++){
-    eventName = `Thank you ${cards[i]}, for the wonderful ${event} gift!`;
+    eventName = `Thank you, ${cards[i]}, for the wonderful ${event} gift!`;
     finalEvent.push(eventName);
   }
   return finalEvent;
